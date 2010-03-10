987c13aaff3da383c6f37d90d1061833e1dabb2b
Yusuke Wada <yusukebe@cpan.org>
2010-03-10 21:35:44 +0900

    initial commit

---
+++ lib/File/Packer.pm
@@ -0,0 +1,32 @@
+package File::Packer;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+File::Packer -
+
+=head1 SYNOPSIS
+
+  use File::Packer;
+
+=head1 DESCRIPTION
+
+File::Packer is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke at kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
